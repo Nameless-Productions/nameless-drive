a9b204ba3ed05c45b2efab80179832263f253dee
Nameless <nameless@thenamelessdev.com>
2026-06-22 11:10:51 +0200

    feat: init project

---
+++ app/layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function RootLayout({children}: {children: React.ReactNode}) {
+  return (<>
+    <html lang="en">
+      <body>
+        <main className="m-3">
+          {children}
+        </main>
+      </body>
+    </html>
+  </>)
+}
