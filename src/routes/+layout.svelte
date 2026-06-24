f75d6735f06dc2483b1d2c5a6c1d17da45d4ddb3
Nameless <nameless@thenamelessdev.com>
2026-06-24 09:07:30 +0200

    update: using sveltekit instad of next js

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import './layout.css';
+
+	let { children } = $props();
+</script>
+
+<svelte:head><link rel="icon" href="https://thenamelessdev.com/logo.svg" /></svelte:head>
+{@render children()}
