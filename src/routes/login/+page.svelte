13aaab61a566a82d6441df870a727c6147009f94
thenamelessdev <nameless@thenamelessdev.com>
2026-07-04 15:30:03 +0200

    feat: add login form todo: do backend for it

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,9 @@
+<p class="font-bold text-xl">Log in</p>
+
+<form action="?/login" method="POST">
+    <input type="text" placeholder="Username" name="username" required class="mt-2">
+    <br>
+    <input type="password" placeholder="Password" name="password" required class="mt-2">
+    <br>
+    <input type="submit" value="Log in" class="mt-2">
+</form>
