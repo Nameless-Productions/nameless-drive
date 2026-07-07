4c377570435d18902c00a5b6dbd6ced41480a379
thenamelessdev <nameless@thenamelessdev.com>
2026-07-07 17:30:33 +0200

    fix: remove download btn from folders

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,7 +19,9 @@
                 <img src="/file.svg" alt="file" class="w-7 h-7">
             {/if}
             <a href={getUrl(c.isFolder, c.id)}>{c.name}</a>
-            <a href={resolve(`/download/${c.id}`)} class="p-1 bg-blue-600 hover:bg-blue-700 duration-300 m-1 rounded-xl cursor-pointer">Download</a>
+            {#if !c.isFolder}
+                <a href={resolve(`/download/${c.id}`)} class="p-1 bg-blue-600 hover:bg-blue-700 duration-300 m-1 rounded-xl cursor-pointer">Download</a>
+            {/if}
             <a href={resolve(`/${c.id}/delete`)} class="ml-auto p-1 bg-red-500 hover:bg-red-400 duration-300 m-1 rounded-xl cursor-pointer">Delete</a>
         </div>
     {/each}
