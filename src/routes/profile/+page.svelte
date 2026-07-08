fdb561348b8b9c015dcb61b65d0c2eca0635e1de
thenamelessdev <nameless@thenamelessdev.com>
2026-07-08 21:17:42 +0200

    feat: add manage apis and logout links to profile page

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,2 +1,14 @@
-<script lang="ts">  
+<script lang="ts">
+	import { resolve } from '$app/paths';
+
+  
+    let {data} = $props();
 </script>
+
+<p class="font-bold text-xl">Hello, {data.user?.username}!</p>
+<br>
+
+<ul class="list-disc list-inside ml-3">
+    <li><a class="link" href={resolve("/profile/api")}>Manage API keys</a></li>
+    <li><a class="link" href={resolve("/profile/logout")}>Log out</a></li>
+</ul>
