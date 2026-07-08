82ecb785499fdb6fafeceacebc5c9d1b3106a3e3
thenamelessdev <nameless@thenamelessdev.com>
2026-07-08 21:48:04 +0200

    feat: verify api key function

---
+++ src/lib/apiUtils.ts
@@ -0,0 +1,21 @@
+import jwt from "jsonwebtoken"
+import { db } from "./db";
+
+export async function verifyApiKey(key: string) {
+    const decodedJwt = jwt.decode(key) as {uid?: number};
+    if(!decodedJwt.uid || isNaN(decodedJwt.uid)) return false;
+
+    const userDB = await db.user.findUnique({
+        where: {
+            id: decodedJwt.uid
+        }
+    });
+    if(!userDB) return false;
+
+    try {
+        const verifyKey = jwt.verify(key, userDB.apiSecret) as {uid: number};
+        return verifyKey.uid;
+    } catch {
+        return false;
+    }
+}
