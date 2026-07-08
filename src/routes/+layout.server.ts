6437fd39dec0371ae4db828045518899d64d0583
thenamelessdev <nameless@thenamelessdev.com>
2026-07-08 14:08:12 +0200

    feat: pass user local as prop in layout

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,12 +1,12 @@
 import { db } from "$lib/db";
 import type { LayoutServerLoad } from "./$types";
 
-export const load: LayoutServerLoad = async () => {
+export const load: LayoutServerLoad = async ({locals}) => {
     let isSetup = true
     const users = await db.user.findMany();
 
     if (users.length === 0) isSetup = false;
     
 
-    return {isSetup}
+    return {isSetup, user: locals.user}
 }
