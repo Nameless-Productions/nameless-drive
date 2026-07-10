661fbcd63e18321fc564933937af97be3b4136c2
thenamelessdev <nameless@thenamelessdev.com>
2026-07-10 14:09:19 +0200

    feat: add ability to create api keys

---
+++ src/routes/profile/api/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { resolve } from "$app/paths";
+
+</script>
+<p class="font-bold text-xl">Manage API keys</p>
+<br>
+
+<a href={resolve("/profile/api/create")} class="link">Create API key</a>
+<br>
+<a href={resolve("/profile/api/delete")} class="link">Invalidate all API keys</a>
---
+++ src/routes/profile/api/create/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    let {data} = $props();
+</script>
+
+<p class="font-bold text-xl">Create API key</p>
+
+{#if data.newApiKey}
+    <p>Your API key: <br>{data.newApiKey}</p>
+    {:else}
+    <p>No API key was returned. Try again.</p>
+{/if}
