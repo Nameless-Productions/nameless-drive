0d33bfc89c056fbbd20217ac3683dcef7b4b0271
thenamelessdev <nameless@thenamelessdev.com>
2026-07-10 16:44:42 +0200

    feat: add download api

---
+++ src/routes/api/download/[slug]/+server.ts
@@ -0,0 +1,14 @@
+import { json } from "@sveltejs/kit";
+import type { RequestHandler } from "./$types";
+import { getFile } from "$lib/getFile";
+
+export const GET: RequestHandler = async ({params}) => {
+    const fileId = Number(params.slug);
+
+    if(isNaN(fileId)) return json({error: "File ID is not a number"}, {status: 400});
+
+    const file = await getFile(fileId);
+    if(!file) return json({error: "File doesn't exists or is folder"}, {status: 404});
+
+    return new Response(file)
+}
